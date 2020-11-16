071fe7bfb3fdf39c82cb4de11a78047315548067
JAKHONGIR <52883946+djokk@users.noreply.github.com>
2020-11-16 17:18:48 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.slick-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  prevArrow: '<button class="prev"><img src="./img/prev.png" alt=""></button>',
+  nextArrow: '<button class="next"><img src="./img/next.png" alt=""></button>',
+});
+
+$('.share__slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  variableWidth: true,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  prevArrow: '<button class="prev"><img src="./img/prev.png" alt=""></button>',
+  nextArrow: '<button class="next"><img src="./img/next.png" alt=""></button>',
+});
