072b9c0c68365c0b36bbe0a3bc3ddef45cb76f2c
JAKHONGIR <52883946+djokk@users.noreply.github.com>
2020-11-18 00:15:32 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-$('.slick-slider').slick({
+$('.slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  // autoplay: true,
-  // autoplaySpeed: 2000,
+  autoplay: true,
+  autoplaySpeed: 2000,
   prevArrow: '<button class="prev"><img src="./img/prev.png" alt=""></button>',
   nextArrow: '<button class="next"><img src="./img/next.png" alt=""></button>',
 });
 
 $('.share__slider').slick({
+  speed: 300,
   slidesToShow: 4,
   slidesToScroll: 1,
-  variableWidth: true,
   // autoplay: true,
-  // autoplaySpeed: 2000,
+  autoplaySpeed: 2000,
   prevArrow: '<button class="prev"><img src="./img/prev.png" alt=""></button>',
   nextArrow: '<button class="next"><img src="./img/next.png" alt=""></button>',
 });
+
+// document.querySelector('.red-light').querySelector('.prev').querySelector('img').src = './img/prev-gray.png';
+// document.querySelector('.red-light').querySelector('.next').querySelector('img').src = './img/next-gray.png';
+
+
+// document.querySelector('.red-light .prev img').src = './img/prev-gray.png';
+// document.querySelector('.red-light .next img').src = './img/next-gray.png';
+
+document.querySelectorAll('.red-light .prev img').src = './img/prev-gray.png';
+document.querySelectorAll('.red-light .next img').src = './img/next-gray.png';
